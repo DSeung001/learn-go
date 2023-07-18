39f4b92cdce74f6cd6d36523535827631c5a7793
지승렬 <hsnc23032108@hanwha.com>
2023-07-18 13:41:52 +0900

    channels

--- chanel/main.go
+++ chanel/main.go
@@ -1,18 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+import "fmt"
 
-func say(s string) {
-	for i := 0; i < 5; i++ {
-		time.Sleep(100 * time.Millisecond)
-		fmt.Println(s)
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
 	}
+	c <- sum
 }
 
 func main() {
-	go say("world!")
-	say("hello")
+	s := []int{1, 4, 5, 7, -2, 3, 5}
+
+	c := make(chan int)
+	go sum(s[:len(s)/2], c)
+	go sum(s[len(s)/2:], c)
+	x, y := <-c, <-c
+
+	fmt.Println(x, y, x+y)
 }
