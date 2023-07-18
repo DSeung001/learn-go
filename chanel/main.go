18d0c7ffd542463c00992b1f1304a1c43aaec124
지승렬 <hsnc23032108@hanwha.com>
2023-07-18 13:56:20 +0900

    Range and Close

--- chanel/main.go
+++ chanel/main.go
@@ -2,27 +2,24 @@ package main
 
 import "fmt"
 
-func sum(s []int, c chan int) {
-	sum := 0
-	for _, v := range s {
-		sum += v
+func fibonacci(n int, c chan int) {
+	x, y := 0, 1
+	for i := 0; i < n; i++ {
+		c <- x
+		x, y = y, x+y
 	}
-	c <- sum
+	// chan 에 신규 값 방지
+	close(c)
 }
 
 func main() {
-	s := []int{1, 4, 5, 7, -2, 3, 5}
+	c := make(chan int, 10)
+	fmt.Println(cap(c))
 
-	c := make(chan int)
-	go sum(s[:len(s)/2], c)
-	go sum(s[len(s)/2:], c)
-	x, y := <-c, <-c
+	// goroutine 에 chan 사이즈와 채널을 파라미터로
+	go fibonacci(cap(c), c)
 
-	fmt.Println(x, y, x+y)
-
-	ch := make(chan int, 2)
-	ch <- 1
-	ch <- 2
-	fmt.Println(<-ch)
-	fmt.Println(<-ch)
+	for i := range c {
+		fmt.Println(i)
+	}
 }
