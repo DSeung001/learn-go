88d935815bad4827cec590917ce7269dacf599fb
seungryeol <seungryeol156@gmail.com>
2023-08-30 03:43:05 +0900

    정규식 체커가 아닌 url 분석기로 변경

--- regexp-site/route/route.go
+++ regexp-site/route/route.go
@@ -18,17 +18,16 @@ func Start(port int64) {
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
 
+// url인지만 체크하면 컨텐츠가 부족한대 => url을 분석하는 걸로 바꾸자
 func regexpHandler(w http.ResponseWriter, r *http.Request) {
 	var result []string
 	var regexpArr []*regexp.Regexp
-	var regexpReason = []string{
-		"", "",
-	}
 	var patterns = []string{
-		"", "",
+		`^(https?|ftp):\/\/`,
+		`^(?:https?:\/\/)?(?:ftp:\/\/)?((?:[\w-]+\.)+[\w]{2,})`,
+		`[?&]([^=]+)=([^&]*)`,
 	}
 
-	fmt.Println("regexpHandler 실행")
 	url := r.PostFormValue("url")
 
 	for _, pattern := range patterns {
@@ -41,16 +40,17 @@ func regexpHandler(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	for index, regexpObj := range regexpArr {
-		if !regexpObj.MatchString(url) {
-			result = append(result, regexpReason[index])
-		}
+	for _, regexpObj := range regexpArr {
+		// 슬라이스로 오는데 0,1 로 2개 오고 둘이 다른지
+		matchs := regexpObj.FindStringSubmatch(url)[1]
+		result = append(result, matchs)
+
 	}
 
+	w.Header().Set("Content-Type", "application/json")
 	jsonResult, _ := json.Marshal(result)
 	_, err := w.Write(jsonResult)
 	if err != nil {
 		return
 	}
-	w.WriteHeader(http.StatusOK)
 }
