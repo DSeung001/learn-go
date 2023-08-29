860ad3cdf8611fe061241afce1f236b8a411eefb
seungryeol <seungryeol156@gmail.com>
2023-08-29 21:56:23 +0900

    정규식 체커 형태 변경

--- regexp-site/route/route.go
+++ regexp-site/route/route.go
@@ -1,6 +1,7 @@
 package route
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
 	"log"
@@ -10,30 +11,46 @@ import (
 
 func Start(port int64) {
 	router := mux.NewRouter()
+	router.HandleFunc("/regexp", regexpHandler).Methods("POST")
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
-	router.HandleFunc("/regex", regexHandler).Methods("POST")
 
 	fmt.Printf("Listening on http://localhost:%d\n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
 
-func regexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("regexHandler 실행")
+func regexpHandler(w http.ResponseWriter, r *http.Request) {
+	var result []string
+	var regexpArr []*regexp.Regexp
+	var regexpReason = []string{
+		"", "",
+	}
+	var patterns = []string{
+		"", "",
+	}
+
+	fmt.Println("regexpHandler 실행")
 	url := r.PostFormValue("url")
 
-	pattern := "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_\\+.~#()?&//=]*)"
-	regex, err := regexp.Compile(pattern)
+	for _, pattern := range patterns {
+		regexpVal, err := regexp.Compile(pattern)
+		if err != nil {
+			fmt.Println("정규식 오류:", err)
+			return
+		} else {
+			regexpArr = append(regexpArr, regexpVal)
+		}
+	}
+
+	for index, regexpObj := range regexpArr {
+		if !regexpObj.MatchString(url) {
+			result = append(result, regexpReason[index])
+		}
+	}
+
+	jsonResult, _ := json.Marshal(result)
+	_, err := w.Write(jsonResult)
 	if err != nil {
-		fmt.Println("정규식 오류:", err)
 		return
 	}
-
-	if regex.MatchString(url) {
-		fmt.Println("정규식을 통과했습니다.")
-		w.WriteHeader(http.StatusOK)
-
-	} else {
-		fmt.Println("정규식을 통과하지 못했습니다.")
-		w.WriteHeader(http.StatusBadRequest)
-	}
+	w.WriteHeader(http.StatusOK)
 }
