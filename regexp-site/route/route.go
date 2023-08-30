61c16c1ca49e7104058ecc8e97d763b8b3655195
seungryeol <seungryeol156@gmail.com>
2023-08-30 15:03:33 +0900

    주석 추가

--- regexp-site/route/route.go
+++ regexp-site/route/route.go
@@ -10,11 +10,16 @@ import (
 )
 
 func Start(port int64) {
+	// 라우터 생성
 	router := mux.NewRouter()
+
+	// API 매핑
 	router.HandleFunc("/regexp", regexpHandler).Methods("POST")
+	// index.html 매핑
 	router.PathPrefix("/").Handler(http.FileServer(http.Dir("./static/")))
 
 	fmt.Printf("Listening on http://localhost:%d\n", port)
+	// 서버 실행
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), router))
 }
 
@@ -24,14 +29,17 @@ func regexpHandler(w http.ResponseWriter, r *http.Request) {
 
 	url := r.PostFormValue("url")
 
+	// 정규식 유효성 검사
 	regexpVal, err := regexp.Compile(pattern)
 	if err != nil {
 		fmt.Println("정규식 오류:", err)
 		return
 	}
 
+	// 정규식 실행
 	result = regexpVal.FindStringSubmatch(url)
 
+	// 정규식 실행 결과물을 json으로 반환
 	w.Header().Set("Content-Type", "application/json")
 	jsonResult, _ := json.Marshal(result)
 	_, err = w.Write(jsonResult)
