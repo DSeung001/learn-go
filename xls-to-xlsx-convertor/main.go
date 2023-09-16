1a771357878582b928b55ee622415bd219b04efb
seungryeol <seungryeol156@gmail.com>
2023-09-16 18:54:23 +0900

    코드 정리

--- xls-to-xlsx-convertor/main.go
+++ xls-to-xlsx-convertor/main.go
@@ -1,96 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/360EntSecGroup-Skylar/excelize"
-	"github.com/extrame/xls"
-	"log"
-	"os"
-	"xls-to-xlsx-convertor.com/utils"
+	"xls-to-xlsx-convertor.com/xlstoXlsx"
 )
 
 const xlsDirPath = "resources/xls"
 const xlsxDirPath = "resources/xlsx"
 
 func main() {
-	// XLS 파일 경로
-	fileNames := getXlsFileNames()
+	// xls 경로랑 xlsx 경로 전달
+	xlstoXlsx.FileConversion(xlsDirPath, xlsxDirPath)
 
-	for _, fileName := range fileNames {
-		xlsFilePath := xlsDirPath + "/" + fileName
-		var data [][]string
-		var arr []string
-
-		// Excel 파일 열기
-		xlFile, err := xls.Open(xlsFilePath, "utf-8")
-		if err != nil {
-			log.Fatalf("엑셀 파일 열기 실패: %v", err)
-		}
-
-		for i := 0; i < xlFile.NumSheets(); i++ {
-			sheet := xlFile.GetSheet(i)
-			// 행 순회
-			for rowIdx := 0; rowIdx <= int(sheet.MaxRow); rowIdx++ {
-				row := sheet.Row(rowIdx)
-
-				data = append(data, arr)
-
-				if row != nil {
-					// 열 순회
-					for colIdx := 0; colIdx < row.LastCol(); colIdx++ {
-						cell := row.Col(colIdx)
-						data[rowIdx] = append(data[rowIdx], cell)
-					}
-				}
-			}
-		}
-
-		utils.HandleErr(createXLSXFile(data, xlsxDirPath+"/"+fileName+"x"))
-	}
-
-}
-
-func createXLSXFile(data [][]string, fileName string) error {
-	// 파일 생성
-	file := excelize.NewFile()
-
-	for rowIndex, row := range data {
-		for colIndex, cellValue := range row {
-			// A1 형태로 위치 값 생성
-			cellPosition := excelize.ToAlphaString(colIndex) + fmt.Sprintf("%d", rowIndex+1)
-			file.SetCellValue("Sheet1", cellPosition, cellValue)
-		}
-	}
-
-	// 시트 저장
-	if err := file.SaveAs(fileName); err != nil {
-		return err
-	}
-	return nil
-}
-
-// getXlsFileNames : xls 파일 이름 가져오기
-func getXlsFileNames() []string {
-	var fileNames []string
-
-	// 폴더 열기
-	dir, err := os.Open(xlsDirPath)
-	utils.HandleErr(err)
-	defer func(dir *os.File) {
-		err := dir.Close()
-		utils.HandleErr(err)
-	}(dir)
-
-	// 디렉토리 내 파일 목록 읽기
-	fileInfos, err := dir.Readdir(-1)
-	utils.HandleErr(err)
-
-	// 파일 이름을 리스트에 넣기
-	for _, fileInfo := range fileInfos {
-		if !fileInfo.IsDir() {
-			fileNames = append(fileNames, fileInfo.Name())
-		}
-	}
-
-	return fileNames
 }
---
+++ xls-to-xlsx-convertor/xlstoXlsx/functions.go
@@ -0,0 +1,92 @@
+package xlstoXlsx
+
+import (
+	"fmt"
+	"github.com/360EntSecGroup-Skylar/excelize"
+	"github.com/extrame/xls"
+	"log"
+	"os"
+	"xls-to-xlsx-convertor.com/utils"
+)
+
+// FileConversion : xlsDirPath 경로에 파일들을 xlsx 로 바꿔서 xlsxDirPath 에 저장
+func FileConversion(xlsDirPath string, xlsxDirPath string) {
+	fileNames := getFileNames(xlsDirPath)
+
+	for _, fileName := range fileNames {
+		xlsFilePath := xlsDirPath + "/" + fileName
+		var data [][]string
+		var arr []string
+
+		// Excel 파일 열기
+		xlFile, err := xls.Open(xlsFilePath, "utf-8")
+		if err != nil {
+			log.Fatalf("엑셀 파일 열기 실패: %v", err)
+		}
+
+		for i := 0; i < xlFile.NumSheets(); i++ {
+			sheet := xlFile.GetSheet(i)
+			// 행 순회
+			for rowIdx := 0; rowIdx <= int(sheet.MaxRow); rowIdx++ {
+				row := sheet.Row(rowIdx)
+
+				data = append(data, arr)
+
+				if row != nil {
+					// 열 순회
+					for colIdx := 0; colIdx < row.LastCol(); colIdx++ {
+						cell := row.Col(colIdx)
+						data[rowIdx] = append(data[rowIdx], cell)
+					}
+				}
+			}
+		}
+		utils.HandleErr(createXLSXFile(data, xlsxDirPath+"/"+fileName+"x"))
+	}
+}
+
+// getFileNames : dirPath 에 있는 파일 이름 가져오기
+func getFileNames(dirPath string) []string {
+	var fileNames []string
+
+	// 폴더 열기
+	dir, err := os.Open(dirPath)
+	utils.HandleErr(err)
+	defer func(dir *os.File) {
+		err := dir.Close()
+		utils.HandleErr(err)
+	}(dir)
+
+	// 디렉토리 내 파일 목록 읽기
+	fileInfos, err := dir.Readdir(-1)
+	utils.HandleErr(err)
+
+	// 파일 이름을 리스트에 넣기
+	for _, fileInfo := range fileInfos {
+		if !fileInfo.IsDir() {
+			fileNames = append(fileNames, fileInfo.Name())
+		}
+	}
+
+	return fileNames
+}
+
+// createXLSXFile : XLS 파일 만들기
+func createXLSXFile(data [][]string, fileName string) error {
+	// 파일 생성
+	file := excelize.NewFile()
+
+	for rowIndex, row := range data {
+		for colIndex, cellValue := range row {
+			// A1 형태로 위치 값 생성
+			cellPosition := excelize.ToAlphaString(colIndex) + fmt.Sprintf("%d", rowIndex+1)
+			file.SetCellValue("Sheet1", cellPosition, cellValue)
+		}
+	}
+
+	// 시트 저장
+	if err := file.SaveAs(fileName); err != nil {
+		return err
+	}
+	return nil
+}
