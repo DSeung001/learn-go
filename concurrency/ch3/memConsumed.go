d544ca190fed8768c16b7792f739bf0dc8f6658e
seungryeol <seungryeol156@gmail.com>
2024-01-01 21:26:18 +0900

    ch3 메모리 양 측정

---
+++ concurrency/ch3/memConsumed.go
@@ -0,0 +1,30 @@
+package ch3
+
+import (
+	"fmt"
+	"runtime"
+	"sync"
+)
+
+func MemConsumed() {
+	memConsumed := func() uint64 {
+		runtime.GC() // GC를 강제로 실행
+		var s runtime.MemStats
+		runtime.ReadMemStats(&s)
+		return s.Sys
+	}
+
+	var c <-chan interface{}
+	var wg sync.WaitGroup
+	noop := func() { wg.Done(); <-c }
+
+	const numGoroutines = 1e4
+	wg.Add(numGoroutines)
+	before := memConsumed()
+	for i := numGoroutines; i > 0; i-- {
+		go noop()
+	}
+	wg.Wait()
+	after := memConsumed()
+	fmt.Printf("%.3fkb", float64(after-before)/numGoroutines/1000)
+}
--- concurrency/main.go
+++ concurrency/main.go
@@ -1,9 +1,11 @@
 package main
 
-import "concurrency.com/ch1"
+import "concurrency.com/ch3"
 
 func main() {
 	//ch1.Deadlock()
 	//ch1.LiveLock()
-	ch1.GoodAndBadWorker()
+	//ch1.GoodAndBadWorker()
+
+	ch3.MemConsumed()
 }
