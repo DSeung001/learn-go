c8d24808aadf8d654b6c3f7a357bf1c73fcb73e3
seungryeol <seungryeol156@gmail.com>
2024-01-19 03:49:13 +0900

    고루틴 반복문 주의점 blog

--- concurrency/main.go
+++ concurrency/main.go
@@ -1,14 +1,26 @@
 package main
 
-import "concurrency.com/ch1"
+import (
+	"fmt"
+	"sync"
+)
 
 func main() {
 	//ch1.Deadlock()
 	//ch1.LiveLock()
-	ch1.GoodAndBadWorker()
+	//ch1.GoodAndBadWorker()
 	//ch1.RaceCondition()
 	//ch1.RaceConditionSolution()
 
 	//ch3.MemConsumed()
 
+	var wg sync.WaitGroup
+	for _, text := range []string{"a", "b", "c"} {
+		wg.Add(1)
+		go func(text string) {
+			defer wg.Done()
+			fmt.Println(text)
+		}(text)
+	}
+	wg.Wait()
 }
