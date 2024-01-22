f86d511f60e5b1786ed20b0e2583455db05924ed
seungryeol <seungryeol156@gmail.com>
2024-01-22 21:39:08 +0900

    1018

--- algorithm/1018/main.go
+++ algorithm/1018/main.go
@@ -7,67 +7,52 @@ import (
 	"slices"
 )
 
-// 주소 : https://www.acmicpc.net/problem/1018
-// 체스판의 시작은 맨위가 흰색, 검은색이고
-// 아래 처럼 나와야 함
-// 체스판을 8x8로 자르는데 가장 적게 색칠을 해야하는 경우의수를 구하기
-// WBWBWBWB
-// BWBWBWBW
-
 func main() {
 	var reader = bufio.NewReader(os.Stdin)
 	var n, m int
-	var size int = 8
+	var size = 8
 
 	fmt.Fscan(reader, &n)
 	fmt.Fscanln(reader, &m)
 	var board []string
-	var results []int
 
 	for i := 0; i < n; i++ {
 		var str string
 		fmt.Fscanln(reader, &str)
 		board = append(board, str)
 	}
+	// 최악의 경우의 수로도 넘을 수 없는 값
+	var min = n * m
 
-	// 바둑판 찾기
-	// 왼쪽 부터 차례대로 시작하는 값으로 인덱스를 가져온 후 반복문
-	// 그 반복에서 색칠해야하는 부분 찾는 함수 필요
-
-	// 이렇게 하면 8x8로 자를 때 시작 값을 가질 수 있는데
+	// 크기가 8이므로 8*8 체스판을 만들 수 있는 범위까지만 반복
 	for i := 0; i <= n-size; i++ {
 		for j := 0; j <= m-size; j++ {
+			var stone = [2]byte{'W', 'B'}
 
-			count := 0
-			// 시작값 -1, W면 0, B면 1
-			var last rune
-
-			for c := 0; c < size; c++ {
-				for r := 0; r < size; r++ {
-
-					target := rune(board[i+c][j+r])
-					if r > 0 || c > 0 {
-						// 지금 BBB에서 안됨 => 연속된 경우도 생각해야함
-						if r == 0 && last == target {
-							continue
-						} else if !(last == 'W' && target == 'B') && !(last == 'B' && target == 'W') {
-							fmt.Println(c, r, string(target), string(last))
-							count++
-
-							if last == 'W' {
-								last = 'B'
-							} else if last == 'B' {
-								last = 'W'
-							}
-							continue
-						}
+			// way1: W로 시작하는 체스판
+			way1 := 0
+			for c := i; c < i+size; c++ {
+				for r := j; r < j+size; r++ {
+					if board[c][r] != stone[(c+r)%2] {
+						way1++
 					}
-					last = target
 				}
 			}
-			results = append(results, count)
+
+			// way2: B로 시작하는 체스판
+			way2 := 0
+			for c := i; c < i+size; c++ {
+				for r := j; r < j+size; r++ {
+					if board[c][r] != stone[(c+r+1)%2] {
+						way2++
+					}
+				}
+			}
+
+			// 가장 작은 값 선택
+			var results = []int{min, way1, way2}
+			min = slices.Min(results)
 		}
 	}
-
-	fmt.Println(slices.Min(results))
+	fmt.Println(min)
 }
