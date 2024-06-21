a9a039fb56f95e98457db344c9dc2942ea608899
seungryeol <seungryeol156@gmail.com>
2024-06-21 17:29:56 +0900

    보류 =

--- algorithm/22860/main.go
+++ algorithm/22860/main.go
@@ -7,97 +7,110 @@ import (
 	"strings"
 )
 
-//https://www.acmicpc.net/problem/22860
-
-type fileInfo struct {
-	child  []*fileInfo
-	isFile bool
-	name   string
+// Node 구조체 정의
+type Node struct {
+	Parent   string
+	Name     string
+	IsFolder bool
+	Children map[string]*Node
 }
 
-type fileRequest struct {
-	parentFolderName string
-	fileName         string
-	isFile           uint8
+// NewNode 함수 정의
+func NewNode(parent string, name string, isFolder bool) *Node {
+	return &Node{
+		Parent:   parent,
+		Name:     name,
+		IsFolder: isFolder,
+		Children: make(map[string]*Node),
+	}
 }
 
-func (f *fileInfo) addFile(fileRequest fileRequest) {
-	if fileRequest.parentFolderName == f.name {
-		if fileRequest.isFile == 1 {
-			f.child = append(f.child, &fileInfo{name: fileRequest.fileName, isFile: false})
-		} else {
-			f.child = append(f.child, &fileInfo{name: fileRequest.fileName, isFile: true})
-		}
-	} else {
-		for _, child := range f.child {
-			if !child.isFile {
-				child.addFile(fileRequest)
+// AddChild 메서드 정의
+func (n *Node) AddChild(newNode *Node) {
+	if n.Name == newNode.Parent {
+		n.Children[newNode.Name] = newNode
+		return
+	}
+
+	for _, child := range n.Children {
+		child.AddChild(newNode)
+	}
+}
+
+// CountFiles 메서드 정의
+func (n *Node) CountFiles() (int, int) {
+	fileSet := make(map[string]struct{})
+	var countFilesRecursively func(node *Node)
+
+	countFilesRecursively = func(node *Node) {
+		for name, child := range node.Children {
+			if child.IsFolder {
+				countFilesRecursively(child)
+			} else {
+				fileSet[name] = struct{}{}
 			}
 		}
 	}
+	countFilesRecursively(n)
+	return len(fileSet), len(fileSet)
 }
 
-func findFile(files *fileInfo, path string) (int, int) {
-	var paths = strings.Split(path, "/")
-
-	// 파일 탐색 자체가 잘못됨 => 다시 짜야함
-	for _, path := range paths {
-		if path == files.name && paths[len(path)-1] == files.name {
-			return getNumber(files)
-		} else if len(files.child) > 0 {
-			return findFile(files, path)
-		}
-	}
-	return 0, 0
-}
-
-// 중첩 파일 없애야하고
-func getNumber(files *fileInfo) (int, int) {
-	fileNum, allNum := 0, 0
-	for _, child := range files.child {
-		allNum++
-		if child.isFile {
-			fileNum++
-		} else {
-			num1, num2 := getNumber(child)
-			fileNum += num1
-			allNum += num2
-		}
-	}
-	return fileNum, allNum
-}
-
-func printFileInfo(fileInfo fileInfo, level int) {
-	prefix := ""
-	for i := 0; i < level; i++ {
-		prefix += "  "
-	}
-
-	fmt.Printf("level:%d %s%s (File: %t) \n", level, prefix, fileInfo.name,
-		fileInfo.isFile)
-	for _, child := range fileInfo.child {
-		printFileInfo(*child, level+1)
+// PrintTree 메서드 정의
+func (n *Node) PrintTree(indent string) {
+	fmt.Printf("%s%s (Folder: %t)\n", indent, n.Name, n.IsFolder)
+	for _, child := range n.Children {
+		child.PrintTree(indent + "  ")
 	}
 }
 
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	var fileNum, folderNum, findNum int
-	var fileRequest fileRequest
-	var path string
-	files := &fileInfo{name: "main"}
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	var n, m int
+	fmt.Sscanf(scanner.Text(), "%d %d", &n, &m)
 
-	// 파일 수, 폴더 수 입력
-	fmt.Fscanln(reader, &fileNum, &folderNum)
-	for i := 0; i < fileNum+folderNum; i++ {
-		fmt.Fscanln(reader, &fileRequest.parentFolderName, &fileRequest.fileName, &fileRequest.isFile)
-		files.addFile(fileRequest)
-	}
-	printFileInfo(*files, 0)
+	nodes := NewNode("", "root", true)
 
-	fmt.Fscanln(reader, &findNum)
-	for i := 0; i < findNum; i++ {
-		fmt.Fscanln(reader, &path)
-		fmt.Println(findFile(files, path))
+	for i := 0; i < n; i++ {
+		scanner.Scan()
+		parts := strings.Split(scanner.Text(), " ")
+		parentName := parts[0]
+		childName := parts[1]
+		isFolder := parts[1] != "file"
+
+		nodes.AddChild(NewNode(parentName, childName, isFolder))
 	}
+
+	queries := make([]string, m)
+	for i := 0; i < m; i++ {
+		scanner.Scan()
+		queries[i] = scanner.Text()
+	}
+
+	// 계층적으로 출력
+	nodes.PrintTree("")
+
+	// 쿼리 처리 및 결과 출력
+	for _, query := range queries {
+		node := findNode(nodes, query)
+		if node != nil {
+			fileCount, uniqueFileCount := node.CountFiles()
+			fmt.Printf("%s %d %d\n", query, fileCount, uniqueFileCount)
+		} else {
+			fmt.Printf("%s 0 0\n", query)
+		}
+	}
+}
+
+// findNode 함수 정의
+func findNode(root *Node, name string) *Node {
+	if root.Name == name {
+		return root
+	}
+	for _, child := range root.Children {
+		if found := findNode(child, name); found != nil {
+			return found
+		}
+	}
+	return nil
 }
